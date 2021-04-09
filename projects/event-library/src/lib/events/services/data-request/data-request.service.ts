645a27dad8c3ab4c58758d0011b9f90d3074481d
Pratiksha Khandagale <pratiksha_k@techjoomla.com>
2021-04-09 11:23:04 +0530

    Task #169598 feat: Added post request and event service

---
+++ projects/event-library/src/lib/events/services/data-request/data-request.service.ts
@@ -0,0 +1,93 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { of as observableOf, throwError as observableThrowError, Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { ServerResponse, RequestParam, HttpOptions } from '../../interfaces';
+import * as _ from 'lodash-es';
+import { UserConfigService } from '../userConfig/user-config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  /**
+     * Contains base Url for api end points
+     */
+  baseUrl = this.userConfigService.getConfigUrl().host;
+
+  constructor(
+    private userConfigService: UserConfigService,
+    private http: HttpClient) {
+  }
+
+  /**
+ * for preparing headers
+ */
+  private getHeader(headers?: HttpOptions['headers']): HttpOptions['headers'] {
+    const default_headers = {
+      Accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJRekw4VVA1dUtqUFdaZVpMd1ZtTFJvNHdqWTg2a2FrcSJ9.TPjV0xLacSbp3FbJ7XeqHoKFN35Rl4YHx3DZNN9pm0o',
+    };
+
+    return default_headers;
+  }
+
+  /**
+ * for making post api calls
+ * @param RequestParam requestParam interface
+ */
+  post(requestParam: RequestParam): Observable<any> {
+    const httpOptions: HttpOptions = {
+      headers: requestParam.header ? this.getHeader(requestParam.header) : this.getHeader(),
+      params: requestParam.param
+    };
+    return this.http.post(requestParam.url, requestParam.data, httpOptions).pipe(
+      mergeMap((data: any) => {
+        if (data.responseCode !== 'OK') {
+          return observableThrowError(data);
+        }
+        return observableOf(data);
+      }));
+  }
+
+
+  /**
+ * for making get api calls
+ *
+ * @param requestParam interface
+ */
+  get(requestParam: RequestParam): Observable<any> {
+    const httpOptions: HttpOptions = {
+      headers: requestParam.header ? requestParam.header : this.getHeader(),
+      params: requestParam.param
+    };
+
+    return this.http.get(requestParam.url, httpOptions).pipe(
+      mergeMap((data: any) => {
+
+        return observableOf(data);
+      }));
+
+  }
+
+  /**
+* for making post api calls
+* @param RequestParam requestParam interface
+*/
+  patch(requestParam: RequestParam): Observable<any> {
+    const httpOptions: HttpOptions = {
+      headers: requestParam.header ? this.getHeader(requestParam.header) : this.getHeader(),
+      params: requestParam.param
+    };
+    return this.http.patch(requestParam.url, requestParam.data, httpOptions).pipe(
+      mergeMap((data: any) => {
+        if (data.responseCode !== 'OK') {
+          return observableThrowError(data);
+        }
+        return observableOf(data);
+      }));
+  }
+
+}
+
+
---
+++ projects/event-library/src/lib/events/services/event/event.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { DataService } from '../data-request/data-request.service';
+import { UserConfigService } from '../userConfig/user-config.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventService {
+  isEnroll: boolean = false;
+  items: any;
+  constructor(
+    private userConfigService: UserConfigService,
+    private dataService: DataService) {
+  }
+
+  /**
+   * To user enrolled event list
+   */
+  getEnrollEvents(courseId, userId) {
+
+    const req = {
+      url: this.userConfigService.getConfigUrl().enrollListApi
+    };
+
+    return this.dataService.get(req);
+  }
+
+  /**
+   * For Enroll/Unenroll to the event
+   */
+  enrollToEventPost(action, cId, uId) {
+    const requestBody = {
+      request: {
+        "courseId": cId,
+        "userId": uId,
+        "fixedBatchId": "event_batch_id"
+      }
+    };
+
+    const req = {
+      url: this.userConfigService.getConfigUrl().enrollApi,
+      data: requestBody
+
+    };
+
+    return this.dataService.post(req).subscribe((data) => {
+      console.log("Result = ", data);
+    });
+  }
+
+}
+
+
