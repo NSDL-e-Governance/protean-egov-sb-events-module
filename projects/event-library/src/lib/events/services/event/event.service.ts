2a417ee351351f2097636f7a6dfcf61574a9f0fe
TTPL-RT-017 <ankita_c@techjoomla.com>
2022-02-14 11:15:17 +0530

    Bug #168789 fix: json date in similar events section on event-detail view

--- projects/event-library/src/lib/events/services/event/event.service.ts
+++ projects/event-library/src/lib/events/services/event/event.service.ts
@@ -316,7 +316,6 @@ export class EventService {
  }
 
  async getEventStatus(event) {
-console.log("Event---",event.venue);
   // Event Start date time
   var startEventTime = await this.timezoneCal.calcTime(event.startDate, event.startTime);
   var startDifference = startEventTime.getTime() - this.todayDateTime.getTime();
@@ -349,18 +348,6 @@ console.log("Event---",event.venue);
     event.showDate = 'Ended On ' +  this.datepipe.transform(event.endDate, 'longDate') + ', ' + this.datepipe.transform(endEventTime, 'HH:mm') + ' (' + timezoneshort + ')';
   }
   
-  
-  if(event.venue != 'null')
-  {
-    var array = JSON.parse("[" + event.venue + "]");
-    event.venue = array[0].name;
-  }
-  else
-  {
-    event.venue = '-';
-  }
-  
-  console.log(event,"ET");
   return event;
 }
 }
